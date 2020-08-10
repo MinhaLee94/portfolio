b84707c4ce70cda12a088cf97452722c4fb277b9
MinhaLee94 <67772844+shjune10@users.noreply.github.com>
2020-08-10 18:02:03 +0900

    JS: navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', () => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
